d4daa7a4d0fb8377660f4dd980dd1594fc96d016
Dan Peddle <dan@flarework.com>
2024-11-22 12:26:16 +0000

    collections

--- src/03_collection.ts
+++ src/03_collection.ts
@@ -15,11 +15,15 @@ export interface Dictionary<T> {
 }
 
 interface ArrayForEachIteratee<T> {
-  (value?: T, index?: number, collection?: Array<T>): any;
+  (value?: T,
+   index?: number,
+   collection?: Array<T>): any;
 }
 
 interface DictionaryForEachIteratee<T> {
-  (value?: T, key?: string, collection?: Dictionary<T>): any;
+  (value?: T,
+   key?: string,
+   collection?: Dictionary<T>): any;
 }
 
 /**
@@ -45,12 +49,37 @@ interface DictionaryForEachIteratee<T> {
  *  _.forEach(collection, iteratee); => result === [['0', 'first'], ['1', 'second'], ['2', 'thidrd']];
  *
  */
-export function forEach() {
+export function forEach<T>(collection:Dictionary<T>, iteratee: DictionaryForEachIteratee<T>): void
+export function forEach<T>(collection:T[], iteratee: ArrayForEachIteratee<T>): void
+
+export function forEach<T>(
+  collection: T[] | Dictionary<T>,
+  iteratee: ArrayForEachIteratee<T> | DictionaryForEachIteratee<T>
+): void {
+
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i ++) {
+      (iteratee as ArrayForEachIteratee<T>)(collection[i], i, collection)
+    }
+  } else {
+    for (let k in collection) {
+      (iteratee as DictionaryForEachIteratee<T>)(collection[k], k, collection)
+    }
+  }
+
+
 }
 
-interface EveryIteratee<T> {
+interface ArrayEveryIteratee<T> {
   (value?: T, index?: number, collection?: Array<T>): boolean;
 }
+
+interface DictionaryEveryIteratee<T> {
+  (value?: T,
+   key?: string,
+   collection?: Dictionary<T>): boolean;
+}
+
 
 /**
  * ### every
@@ -64,7 +93,27 @@ interface EveryIteratee<T> {
  *  _.every([true, 1, null, 'yes'], Boolean); => false
  *  _.every([null, null, null], (value, index, collection) => value === null); => true
  */
-export function every() {
+export function every<T>(collection: Dictionary<T>, iteratee: DictionaryEveryIteratee<T>): boolean
+export function every<T>(collection: T[], iteratee: ArrayEveryIteratee<T>): boolean
+export function every<T>(
+  collection: T[] | Dictionary<T>, 
+  iteratee: ArrayEveryIteratee<T> | DictionaryEveryIteratee<T>
+): boolean {
+  if (Array.isArray(collection)) {
+    for (let i = 0; i< collection.length; i++) {
+      if ((iteratee as ArrayEveryIteratee<T>)(collection[i], i, collection) === false) {
+        return false
+      }
+    }
+  } else {
+    for (let k in collection) {
+      if ((iteratee as DictionaryEveryIteratee<T>)(collection[k], k, collection) === false) {
+        return false
+      }
+    }
+  }
+
+  return true;
 }
 
 /**
@@ -88,7 +137,53 @@ export function every() {
  *  _.filter<number>(collection, iteratee) => { 'a': 1, 'c': 3 }
  *
  */
-export function filter() {
+
+export interface FilterIteratee<T> {
+  (value: T): boolean
+}
+
+export interface DictionaryFilterIteratee<T> {
+  (value: T,
+   key?: string): boolean
+}
+
+
+export function filter<T>(collection: T[], iteratee:FilterIteratee<T>) : T[]
+export function filter<T>(collection: Dictionary<T>, iteratee: DictionaryFilterIteratee<T>): Dictionary<T>
+export function filter<T>(
+  collection: T[] | Dictionary<T>,
+  iteratee: FilterIteratee<T> | DictionaryFilterIteratee<T>
+): T[] | Dictionary<T> {
+  if (Array.isArray(collection)) {
+    const nextCollection: T[] = [];
+
+    for (let i = 0; i < collection.length; i++) {
+      const val = collection[i];
+      if (iteratee(val)) {
+        nextCollection.push(val)
+      }
+    }
+    return nextCollection;
+  } else {
+    const nextCollection: Dictionary<T> = {};
+
+    for (let k in collection) {
+      const val = collection[k];
+      if (iteratee(val, k)) {
+        nextCollection[k] = val;
+      }
+    }
+    return nextCollection;
+  }
+
+}
+
+interface ArrayMapIteratee<T> {
+  (value: T) : T
+}
+
+interface DictionaryMapIteratee<T> {
+  (value: T, key?: string): T[]
 }
 
 /**
@@ -110,7 +205,31 @@ export function filter() {
  *
  *  _.map<number>(collection, iteratee) => [[1,'a'], [2, 'b']]
  */
-export function map() {
+
+export function map<T>(collection: T[], iteratee: ArrayMapIteratee<T>): T[]
+export function map<T>(collection: Dictionary<T>, iteratee: DictionaryMapIteratee<T>): Dictionary<T>
+
+export function map<T>(
+  collection: T[] | Dictionary<T>,
+  iteratee: ArrayMapIteratee<T> | DictionaryMapIteratee<T>
+): T[] {
+  if (Array.isArray(collection)) {
+    const nextCollection = [];
+
+    for (let i = 0; i < collection.length; i++) {
+      nextCollection.push((iteratee as ArrayMapIteratee<T>)(collection[i]))
+    }
+
+    return nextCollection;
+  } else {
+    const nextCollection = [];
+
+    for (let k in collection) {
+      nextCollection.push((iteratee as DictionaryMapIteratee<T>)(collection[k], k))
+    }
+
+    return nextCollection;
+  }
 }
 
 /**
@@ -133,5 +252,36 @@ export function map() {
  *    }, {}); => { '1': ['a', 'c'], '2': ['b'] }
  *
  */
-export function reduce() {
+
+interface ArrayReducer<T> {
+  (accumulator: T, value: T):T
+}
+
+interface DictionaryReducer<T> {
+  (accumulator: T, value: T, key?: string): T
+}
+
+export function reduce<T>(collection: T[], reducer: ArrayReducer<T>, seed: T): T
+export function reduce<T>(collection: Dictionary<T>, reducer: DictionaryReducer<T>, seed: T): T
+
+export function reduce<T>(
+  collection: T[] | Dictionary<T>,
+  reducer: ArrayReducer<T> | DictionaryReducer<T>,
+  seed: T
+): T {
+
+  let toReturn = seed;
+
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      toReturn = reducer(toReturn, collection[i]);
+    }
+
+  } else {
+    for (let k in collection) {
+      toReturn = reducer(toReturn, collection[k], k)
+    }
+  }
+
+  return toReturn;
 }
